0f67604eaa5353f5f044bd2d514bb01a34d03989
zhukewen <stevekewen@163.com>
2022-11-28 22:22:56 -0500

    some change

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const data = require('../data');
 const userData = data.users;
 const path = require('path');
 const helpers = require('../helpers');
+const test = require('../test')
 
 router.route("/").get(async (req, res) => {
   //code here for GET
@@ -13,7 +14,7 @@ router.route("/").get(async (req, res) => {
 
 router.route("/search").post(async (req, res) => {
   //code here for POST
-  let name = re.body.searchUserNameInputText
+  let name = req.body.searchUserNameInputText
   try {
     name = helpers.validUserName(name)
   } catch(e) {
@@ -69,18 +70,7 @@ router.route("/id/:id").get(async (req, res) => {
 
 router.route("/test").get(async (req, res) => {
   //code here for GET
-  const user = {
-    "userID" : "507f1f77bcf86cd799439011",
-    "userName" : "Alexia Dua",
-    "email" : "Alexia@gmail.com",
-    "birthDate" : "05/06/2000",
-    "harshPassword" : "$2a$08$XdvNkNIL8F8xsuIUeSbNOF",
-    "reviews" : ["507f1f77bcf86cd799439012", "507f1f77bcf86cd799439013"],
-    "comments" : ["607f1f77bcf86cd998739013"],
-    "ParksHaveVisited" : ["7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310","507f1f77bcf86cd799439011"],
-    "ParksWishToGo" : ["7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6311","7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6311"],
-    "likes" : ["507f1f77bcf86cd799439013","507f1f77bcf86cd799439012","603d992b919a503b9afb856e"]
-  }
+  const user = test.DUMMY_USER
   res.render('profile', {user: user});
   res.status(200)
   return
---
+++ test.js
@@ -0,0 +1,16 @@
+const DUMMY_USER = {
+    "userID" : "507f1f77bcf86cd799439011",
+    "userName" : "Alexia Dua",
+    "email" : "Alexia@gmail.com",
+    "birthDate" : "05/06/2000",
+    "harshPassword" : "$2a$08$XdvNkNIL8F8xsuIUeSbNOF",
+    "reviews" : ["507f1f77bcf86cd799439012", "507f1f77bcf86cd799439013"],
+    "comments" : ["607f1f77bcf86cd998739013"],
+    "ParksHaveVisited" : ["7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310","507f1f77bcf86cd799439011"],
+    "ParksWishToGo" : ["7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6311","7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6311"],
+    "likes" : ["507f1f77bcf86cd799439013","507f1f77bcf86cd799439012","603d992b919a503b9afb856e"]
+  }
+
+  module.exports = {
+    DUMMY_USER
+  };
