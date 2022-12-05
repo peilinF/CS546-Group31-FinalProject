3435faba7c13ae3c2070ca021272c96f1fcc8677
Leona <huangsylvia822@gmail.com>
2022-12-05 03:46:07 -0500

    config session, config app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const static = express.static(__dirname + '/public');
+const session = require('express-session');
 
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
