60defff87992b5696024077f1a36083639553ca2
Leona <huangsylvia822@gmail.com>
2022-12-07 18:22:46 -0500

    Update delete review

--- routes/review.js
+++ routes/review.js
@@ -35,10 +35,49 @@ router
     }
 
     try{
-      const userReviews = await reviewData.getAllUserReviews(userID);
+      const userReviews = await reviewData.getAllUserReviews(req.params.userID);
       res.status(400).render('../views/singlePark',{error:e,  title:"Park Reviews", park:userReviews.userID});
     }catch(e){
       res.status(404).json({ error: 'review by user id not found' });
+    }
+  })
+  .post(async (req, res) => {
+    const info = req.body;
+    if(!info.reviewTitle || !info.content || !info.rating){
+      res.status(400).json({ message: 'All fields need to have valid values' });
+      return;
+    }
+
+    try{
+      req.params.parkID = helper.validParkId(req.params.parkID);
+    }catch(e){
+      return res.status(400).json({error: e});
+    }
+
+    try{
+      req.params.userID = helper.isValidObjectId(req.params.userID);
+    }catch(e){
+      return res.status(400).json({error: e});
+    }
+
+    try{
+      if(typeof info.reviewTitle !== 'string') throw 'reviewTitle must be a string';
+      info.reviewTitle = info.reviewTitle.trim();
+      if(typeof info.content !== 'string') throw 'content must be a string';
+      info.content = info.content.trim();
+      if (typeof info.rating !== 'number') throw 'Rating must be a number';
+      if (info.rating < 0 || info.rating > 5) throw 'Rating must be between 0 and 5';
+      info.rating = info.rating.trim();
+    }catch(e){
+      return res.status(400).json({error: e});
+    }
+
+    try{
+      const {reviewTitle,content,rating} = info;
+      const createReview = await reviewData.createReview(req.params.parkID,req.params.userID,reviewTitle,content,rating);
+      res.status(400).render('../views/singlePark',{error:e,  title:"Park Reviews", park:createReview.parkID});
+    }catch(e){
+      return res.status(400).json({error: e});
     }
   });
 
@@ -52,9 +91,31 @@ router
     }
 
     try{
-      const userReviews = await reviewData.getAllUserReviews(userID);
-      res.status(400).render('../views/singlePark',{error:e,  title:"Park Reviews", park:userReviews.userID});
+      const Reviews = await reviewData.getReview(req.params.reviewID);
+      res.status(400).render('../views/singlePark',{error:e,  title:"Park Reviews", park:Reviews.reviewID});
     }catch(e){
       res.status(404).json({ error: 'review by user id not found' });
     }
-  });
+  })
+  .delete(async (req, res) => {
+    //code here for DELETE
+    try{
+      req.params.reviewID = helper.checkReviewID(reviewID);
+    }catch(e){
+      return res.status(400).json({error: e});
+    }
+
+    try{
+      await reviewData.getReview(req.params.reviewID);
+    }catch(e){
+      return res.status(404).json({error: 'Review not found'});
+    }
+
+    try {
+      await reviewData.removeReview(req.params.reviewID);
+      res.status(200).json({"reviewId": req.params.movieId, "deleted": true});
+    } catch (e) {
+      res.status(500).json({error: e});
+    }
+  })
+  
