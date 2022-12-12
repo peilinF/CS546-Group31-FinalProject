c88923eec3b3d0e187c7c3e1ae165a742020eb01
unknown <steve1998kewen@gmail.com>
2022-12-12 02:20:12 -0500

    implement hide park list btn

--- public/js/park.js
+++ public/js/park.js
@@ -1,81 +1,92 @@
-const parkNameList = 
-[
-    "Denali National Park & Preserve",
-    "Gates Of The Arctic National Park & Preserve",
-    "Glacier Bay National Park & Preserve",
-    "Katmai National Park & Preserve",
-    "Kenai Fjords National Park",
-    "Kobuk Valley National Park",
-    "Lake Clark National Park & Preserve",
-    "Wrangell - St Elias National Park & Preserve",
-    "Crater Lake National Park",
-    "Mount Rainier National Park",
-    "North Cascades National Park",
-    "Olympic National Park",
-    "Channel Islands National Park",
-    "Death Valley National Park",
-    "Joshua Tree National Park",
-    "Sequoia & Kings Canyon National Parks",
-    "Lassen Volcanic National Park",
-    "Pinnacles National Park",
-    "Redwood National and State Parks",
-    "Yosemite",
-    "Arches",
-    "Big Bend",
-    "Bryce Canyon",
-    "Canyonlands",
-    "Capitol Reef",
-    "Carlsbad Caverns",
-    "Grand Canyon",
-    "Great Basin",
-    "Guadalupe Mountains",
-    "Petrified Forest",
-    "Saguaro",
-    "White Sands",
-    "Zion",
-    "Black Canyon",
-    "Grand Teton",
-    "Great Sand Dunes",
-    "Glacier National Park",
-    "Mesa Verde",
-    "Rocky Mountain",
-    "Yellowstone",
-    "Acadia",
-    "Badlands",
-    "Cuyahoga Valley",
-    "Gateway Arch",
-    "Hot Springs",
-    "Indiana Dunes",
-    "Isle Royale",
-    "Theodore Roosevelt",
-    "Voyageurs",
-    "Wind Cave",
-    "Biscayne",
-    "Congaree",
-    "Dry Tortugas",
-    "Everglades",
-    "Great Smoky Mountain",
-    "Mammoth Cave",
-    "Shenandoah",
-    "New River Gorge",
-    "Haleakala",
-    "Hawai'i Volcanoes",
-    "America Samoa",
-    "Virgin Islands",
+const parkNameList = [
+  "Denali National Park & Preserve",
+  "Gates Of The Arctic National Park & Preserve",
+  "Glacier Bay National Park & Preserve",
+  "Katmai National Park & Preserve",
+  "Kenai Fjords National Park",
+  "Kobuk Valley National Park",
+  "Lake Clark National Park & Preserve",
+  "Wrangell - St Elias National Park & Preserve",
+  "Crater Lake National Park",
+  "Mount Rainier National Park",
+  "North Cascades National Park",
+  "Olympic National Park",
+  "Channel Islands National Park",
+  "Death Valley National Park",
+  "Joshua Tree National Park",
+  "Sequoia & Kings Canyon National Parks",
+  "Lassen Volcanic National Park",
+  "Pinnacles National Park",
+  "Redwood National and State Parks",
+  "Yosemite",
+  "Arches",
+  "Big Bend",
+  "Bryce Canyon",
+  "Canyonlands",
+  "Capitol Reef",
+  "Carlsbad Caverns",
+  "Grand Canyon",
+  "Great Basin",
+  "Guadalupe Mountains",
+  "Petrified Forest",
+  "Saguaro",
+  "White Sands",
+  "Zion",
+  "Black Canyon",
+  "Grand Teton",
+  "Great Sand Dunes",
+  "Glacier National Park",
+  "Mesa Verde",
+  "Rocky Mountain",
+  "Yellowstone",
+  "Acadia",
+  "Badlands",
+  "Cuyahoga Valley",
+  "Gateway Arch",
+  "Hot Springs",
+  "Indiana Dunes",
+  "Isle Royale",
+  "Theodore Roosevelt",
+  "Voyageurs",
+  "Wind Cave",
+  "Biscayne",
+  "Congaree",
+  "Dry Tortugas",
+  "Everglades",
+  "Great Smoky Mountain",
+  "Mammoth Cave",
+  "Shenandoah",
+  "New River Gorge",
+  "Haleakala",
+  "Hawai'i Volcanoes",
+  "America Samoa",
+  "Virgin Islands",
 ];
 
-let parkListButton = document.getElementById("get-park-list-btn")
-parkListButton.addEventListener("click", async () => {
-    let parkListContainer = document.querySelector(".park-list")
+let parkListButton = document.getElementById("get-park-list-btn");
+parkListButton.addEventListener("click", (e) => {
+  if (e.target.innerText === "Get Park List") {
+    let parkListContainer = document.querySelector(".park-list");
+    let parkListResult = document.createElement("div")
+    parkListResult.classList.add("park-list-result")
     parkNameList.forEach((parkName) => {
-        let parkContainer = document.createElement("div")
-        parkContainer.classList.add("park-item")
+      let parkContainer = document.createElement("div");
+      parkContainer.classList.add("park-item");
 
-        let parkNameElement = document.createElement("h3")
-        parkNameElement.innerText = parkName
+      let parkNameElement = document.createElement("h5");
+      parkNameElement.innerText = parkName;
 
-        parkContainer.appendChild(parkNameElement)
+      parkContainer.appendChild(parkNameElement);
 
-        parkListContainer.appendChild(parkContainer)
+      parkListResult.appendChild(parkContainer);
     });
-})
+    parkListContainer.appendChild(parkListResult)
+    e.target.innerText = "Hide Park List";
+    e.target.style.backgroundColor = "pink";
+  } else {
+    let parkListResult = document.querySelector(".park-list-result")
+    parkListResult.remove()
+    e.target.innerText = "Get Park List";
+    e.target.style.backgroundColor = "#839788";
+  }
+});
