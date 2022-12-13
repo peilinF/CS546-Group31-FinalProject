68a7c8769fc9d5d02e2d15af84478b2a22a5cb17
stevezkw1998 <478121254@qq.com>
2022-12-13 14:27:24 -0500

    fix bug in login profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data');
 const helper = require("../helpers.js");
-const usersData = data.users;
+const userData = data.users;
 const path = require('path');
 const xss = require('xss');
 
@@ -11,10 +11,22 @@ router
   .get(async (req, res) => {
     //code here for GET
     if(req.session.user){
-      res.render('profile',{ user: req.session.user, title:"Successfully authenticated!"});
+      const userEmail = xss(req.session.user.email)
+      try {
+        const user = await userData.getUserByEmail(userEmail)
+        res.status(200).render('profile',{ user: user, title:"Successfully authenticated!"});
+      } catch (e) {
+        if (e === "User not found") {
+          res.status(404).render('error', {path: '/profile', statucode: 404, error: e})
+          return;
+        } else {
+          res.status(400).render('error', {path: '/profile', statucode: 400, error: e})
+        }
+      }
     }else{
       error = "Error page saying that the user is not logged in";
       res.status(401).render('../views/forbiddenAccess',{error:error, title:"Non-Authenticated!"});
+      return
     }
   });
 
