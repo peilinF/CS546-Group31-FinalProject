427797b7fa05d11291645c19b5fe514f650cbec2
Yuehan Qin <64568803+JJthomps-prog@users.noreply.github.com>
2022-12-18 02:03:37 -0500

    Add files via upload

--- routes/home.js
+++ routes/home.js
@@ -1191,7 +1191,7 @@ router.route("/updatepark").post(async (req, res) => {
         if (flag[0] == 1 && flag[1] == 0) {
             await userData.removeParksHaveVisited(user._id, record[0]);
             await userData.addParksWishToGO(user._id, record[0]);
-        } else if (flag[1] == 0 && flag[0] == 1) {
+        } else if (flag[1] == 1 && flag[0] == 0) {
           console.log('It is already in the list');
         } else if (flag[1] == 1 && flag[0] == 1) {
             await userData.removeParksHaveVisited(user._id, record[0]);
